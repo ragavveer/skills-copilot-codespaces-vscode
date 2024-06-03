5d040d38f31d2c4fbc23cb9692c58f1178158e1f
Veeraragavan <ragavveer07@gmail.com>
2024-06-03 08:32:35 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,22 +1,9 @@
-// create web server
+// create web server and listen on port 3000
+// to run: node comments.js
+
 var express = require('express');
 var app = express();
-app.listen(3000);
-console.log('Server is running on port 3000');
-// set the view engine
-app.set('view engine', 'ejs');
-// render the comments view
-app.get('/comments', function(req, res) {
-  res.render('comments');
-});
-// Path: views/comments.ejs
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Comments</title>
-</head>
-<body>
-  <h1>Comments</h1>
-  <p>Welcome to the comments page</p>
-</body>
-</html>
+var bodyParser = require('body-parser');
+
+var comments = [
+  {name: 'John', email: ''}]
